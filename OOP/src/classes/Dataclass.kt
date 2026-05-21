3c6a4c9878c1c3a2b030de9833b39f80baba20d9
Ratha <rathavun449@gmail.com>
2026-05-21 16:25:29 +0700

    add extension function in Dataclass.kt

--- OOP/src/classes/Dataclass.kt
+++ OOP/src/classes/Dataclass.kt
@@ -1,31 +1,74 @@
 package classes
 
-import java.sql.Date
 import java.sql.Timestamp
-import javax.print.attribute.standard.DateTimeAtCreation
+import java.util.stream.IntStream.range
+import kotlin.text.split
+
+enum class Currency{
+    RIEL,USD
+}
 
 data class BankAccount(
     private val accountNumber: String,
     val balance: Double,
     val isFreezed: Boolean,
     val accountType: String,
-    val currency: String,
+    val currency: Currency,
     val createdDate: Timestamp? = Timestamp(System.currentTimeMillis()),
 ){
     fun ShowAccountNumber(): String {
-        val hashAccNum = "****${accountNumber.takeLast(4)}"
-        return hashAccNum
+        //val hashAccNum = "****${accountNumber.takeLast(4)}"
+        return accountNumber
     }
 }
+fun BankAccount.ShowCurrency(): String {
+    return when(currency){
+        Currency.USD  -> "$"
+        Currency.RIEL -> "រ"
+    }
+}
+fun Double.formatter(): String {
+    return "%,.2f".format(this)
+}
+fun BankAccount.formattedAccNo(): String {
+    var accNo = ShowAccountNumber()
+    var pre = ""
+    if(accNo.length <12){
+        var time = 12 - accNo.length
 
+        while (time>0){
+            pre += '0'
+            time -= 1
+        }
+        accNo = pre + accNo
+    }else{
+        throw Exception("Invalid AccNo")
+    }
+    return "${accNo.take(4)}-${accNo.substring(4,8)}-${accNo.takeLast(4)}"
+}
 fun main() {
     val acc = BankAccount(
-        accountNumber = "0987687888",
+        accountNumber = "098768788832",
         balance =10000.00,
         isFreezed = false,
         accountType = "Saving",
-        currency = "USD"
+        currency = Currency.RIEL
+    )
+    val acc1 = BankAccount(
+        accountNumber = "68788833",
+        balance =10000.00,
+        isFreezed = false,
+        accountType = "Saving",
+        currency = Currency.USD
     )
 
-    println(acc.ShowAccountNumber())
+
+    println(acc.balance.formatter() + " " + acc.ShowCurrency())
+    try {
+        println(acc1.formattedAccNo())
+    }catch (e: Exception){
+        println(e.message)
+    }
+
+    println(acc1.balance.formatter() + " " + acc1.ShowCurrency())
 }
